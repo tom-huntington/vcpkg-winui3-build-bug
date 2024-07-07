bcbcabb399fec05901fcf42e8398ca937cb1abc2
tom-huntington <55266932+tom-huntington@users.noreply.github.com>
2024-07-07 12:54:20 +1200

    Add project files.

---
+++ App9/MainWindow.xaml.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+#include "onnx.proto3.pb.h"
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::App9::implementation
+{
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+		
+    }
+}
